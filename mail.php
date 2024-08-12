130607eba0e5e272afa36a1b2ac756562bc07b5c
ShehabSerry <111205286+ShehabSerry@users.noreply.github.com>
2024-08-12 23:45:09 +0300

    NEW MAIL , old is commented out

--- mail.php
+++ mail.php
@@ -13,8 +13,12 @@ $mail = new PHPMailer();
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = "taskify49@gmail.com";                     //SMTP username
-$mail->Password   = "qdvb cmpa phsm mwxm";                          //SMTP password
+
+// OLD $mail->Username   = "taskify49@gmail.com";                     //SMTP username OLD
+// OLD $mail->Password   = "qdvb cmpa phsm mwxm";                          //SMTP password OLD
+
+$mail->Username   = "MiDlancerTeam@gmail.com";                     //SMTP username
+$mail->Password   = "nvrv jfzr kdga ysjw ";                          //SMTP password
 $mail->SMTPSecure = "ssl";                                      //Enable implicit TLS encryption
 $mail->Port       = 465;
 $mail->isHTML(true);                                  //Set email format to HTML
