4effb4c2023de8af967e89c8d64841a73db9d939
Salmamohamed33 <140505053+Salmamohamed33@users.noreply.github.com>
2024-08-13 14:14:45 +0300

    Add files via upload

--- otpuser.php
+++ otpuser.php
@@ -69,6 +69,11 @@ if (isset($_POST['resend']))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>verification page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- bs -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
     <link rel="stylesheet" href="css/otp.css">
 </head>
@@ -95,10 +100,13 @@ if (isset($_POST['resend']))
             <button  type="submit" name="resend" class="resbtn">resend</button>
         </div>
         <br>
+          <?php if(!empty($error)) { ?>
+                  <div class="alert alert-warning" role="alert">
+                      <?php echo $error ?>
+                  </div>
+              <?php } ?>
         <button  type="submit" name="submit" class="verify">Verify</button>
-        <div class="alert alert-warning" role="alert">
-            <?php echo $error ?>
-        </div>
+        
     </div>
 
     </div>
