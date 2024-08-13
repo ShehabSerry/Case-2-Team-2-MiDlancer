c35d016386250a9848d734265cd3c8e20754b652
ShehabSerry <111205286+ShehabSerry@users.noreply.github.com>
2024-08-13 22:09:20 +0300

    NO DUP REQUEST

--- select_project.php
+++ select_project.php
@@ -1,5 +1,6 @@
 <?php
 include "connection.php";
+$error = '';
 $user_id=$_SESSION['user_id'];
 $select="SELECT * FROM `project` WHERE `user_id`=$user_id;";
 // $project_id=$_GET['projrct_id'];
@@ -11,8 +12,21 @@ if(isset($_GET['vfid']))
     if(isset($_POST['submit']))
     {
         $project_id = $_POST['career'];
-        $insert = "INSERT INTO `request` VALUES (NULL, 'pending', '$project_id', '$freelancer_id')";
-        $run_insert = mysqli_query($connect, $insert);
+        $join = "SELECT * FROM `request`
+          JOIN `project` ON `project`.`project_id`=`request`.`project_id`
+          WHERE `request`.`project_id`='$project_id' AND `request`.`freelancer_id` = '$freelancer_id'
+         ";
+        $run_join = mysqli_query($connect, $join);
+        if(mysqli_num_rows($run_join) == 0)
+        {
+            $insert = "INSERT INTO `request` VALUES (NULL, 'pending', '$project_id', '$freelancer_id')";
+            $run_insert = mysqli_query($connect, $insert);
+            header("Location: project_details_client.php?details=$project_id");
+        }
+        else
+        {
+            $error = "Request has already been sent";
+        }
     }
 }
 ?>
@@ -38,49 +52,44 @@ if(isset($_GET['vfid']))
 
   <body> 
     <a href="" class="close"><i class="fa-solid fa-x "></i></a>
-<div class="background">
-    <div class="container-main">
-      <div class="wrapper">
+    <div class="background">
+      <div class="container-main">
+        <div class="wrapper">
           <div class="from-wraapper  Sign-in">
-          <form method="post">
-          <h2>Select a Project</h2>
-
-          <div class="input-group">
-            <select name="career" id="career">
-
-            <?php foreach ($run_select as $data) { ?>
-                <option value="<?php echo $data['project_id']; ?>"><?php echo $data['project_name']; ?></option>
-            <?php } ?>
-              
-            </select>
-        </div>
-          
-    </div>
-   
-    <div class="btns">
-      <div class="buttons">
-        <button class="cssbuttons-io-button addto" name="submit">
-          <a href="#">Add To Team</a>
-          <div class="icon">
-            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                fill="currentColor"></path>
-            </svg>
+            <form method="post">
+              <h2>Select a Project</h2>
+              <div class="input-group">
+                <select name="career" id="career">
+                  <?php foreach ($run_select as $data) { ?>
+                    <option value="<?php echo $data['project_id']; ?>"><?php echo $data['project_name']; ?></option>
+                  <?php } ?>
+                </select>
+              </div>
+              <div class="btns">
+                <div class="buttons">
+                  <button class="cssbuttons-io-button addto" name="submit">
+                    <a href="#">Add To Team</a>
+                    <div class="icon">
+                      <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                          fill="currentColor"></path>
+                      </svg>
+                    </div>
+                  </button>
+                  <a href="add_project.php">Don't have Projects Already? Add a Project</a>
+                </div>
+              </div>
+              <?php if(!empty($error)) { ?>
+                  <div class="alert alert-warning" role="alert">
+                      <?php echo $error ?>
+                  </div>
+              <?php } ?>
+            </form>
           </div>
-        </button>
-          <a href="add_project.php">Don't have Projects Already? Add a Project</a>
+        </div>
       </div>
     </div>
-          <div class="signUp-link">
-              <p> <a href="#" class="signUpBtn-link"></a> </p>
-          </div>
-          </form>
-          </div>
-          </div>
-  </div>   
-</div>
     <script src="main.js"></script>
   </body>
-
 </html>
