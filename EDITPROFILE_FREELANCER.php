5676107fb49595b327f753fe476149cfabbfc1e3
alaa <alaanaguib96@gmail.com>
2024-08-13 21:58:24 +0300

    Update EDITPROFILE_FREELANCER.php

--- EDITPROFILE_FREELANCER.php
+++ EDITPROFILE_FREELANCER.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("connection.php");
 if(isset($_SESSION['freelancer_id'])){
     $freelancer_id=$_SESSION['freelancer_id'];
@@ -42,11 +42,12 @@ if(isset($_POST['update'])){
                             WHERE `freelancer_id`=$freelancer_id ";
         $run_update_freelancer=mysqli_query($connect,$update_freelancer);
         if (!empty($_FILES['image']['name'])) {
-            move_uploaded_file($_FILES['image']['tmp_name'], "img/" . $_FILES['image']['name']);
+            move_uploaded_file($_FILES['image']['tmp_name'], "img/profile/" . $_FILES['image']['name']);
         }
         header("location:freelancer_profile.php");
 
 }}
+?>
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,63 +55,69 @@ if(isset($_POST['update'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link rel="stylesheet" href="css/EDITPROFILE.css">
 </head>
 <body>
-    <div class="profile-container">
+    <div class="profile-container  ">
         <div class="profile-card">
+            <a href="./freelancer_profile.php" class="close"><i class="fa-solid fa-x "></i></a>
             <h2>Edit Your Profile</h2>
             <form class="profile-form" method="POST" enctype="multipart/form-data">
                 <?php foreach($run_select as $edit){ ?>
                 <div class="profile-image">
-                        <img src="<?php echo "img/".$edit['freelancer_image'] ?>" alt="Profile Image" id="image-preview">
-                    <input type="file" id="image" name="image" accept="image/*" onchange="previewImage(event)">
+                        <img src="<?php echo "img/profile/".$edit['freelancer_image'] ?>" alt="Profile Image" id="image-preview">
+                    <!-- <input type="file" id="profile-image" name="image" accept="image/*" onchange="previewImage(event)"> -->
+                    <input type="file" name="image" id="">
                 </div>
 
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" id="name" name="name" value="<?php echo $edit['freelancer_name']?>" placeholder="Your Name">
+                    <input type="text" id="name" name="name" value="<?php echo $edit['freelancer_name'] ?>" placeholder="Your Name">
                 </div>
 
                 <div class="form-group">
                     <label for="job-title">Job Title</label>
                     <input type="text" id="job-title" name="job-title" value="<?php echo $edit['job_title'] ?>" placeholder="Your Job Title">
                 </div>
+                <div class="form-group">
+                    <label for="phone-number">Phone Number</label>
+                    <input type="number" id="phone-number" name="phone" value="<?php echo $edit['phone_number'] ?>" placeholder="Your Phone Number">
+                </div>
 
                 <div class="form-group">
                     <label for="bio">Bio</label>
                     <textarea id="bio" name="bio" placeholder="A short bio about yourself"></textarea>
                 </div>
-
-                <div class="form-group">
+                <div class=" lol">
+                    <div class=" link-group lol">
+                <div class="form-group llll">
                     <label for="price-hr">Price per Hour ($)</label>
-                    <input type="number" id="price-hr" value="<?php echo $edit['price/hr']?>" name="price-hr" placeholder="Hourly Rate">
+                    <input type="number" id="price-hr" name="price-hr" value="<?php echo $edit['price/hr'] ?>" placeholder="Hourly Rate">
                 </div>
 
-                <div class="form-group">
-                    <label for="phone_number">Phone Number</label>
-                    <input type="number" id="phone_number" value="<?php echo $edit['phone_number']?>" name="phone" placeholder="Phone Number">
-                </div>
-
-                <div class="form-group">
+                <div class="form-group llll ">
                     <label for="available-hour">Available Hours per Week</label>
-                    <input type="number" id="available-hour" value="<?php echo $edit['available_hours'] ?>" name="available-hour" placeholder="Hours per Week">
+                    <input type="number" id="available-hour" name="available-hour" value="<?php echo $edit['available_hours'] ?>" placeholder="Hours per Week">
                 </div>
+            </div>
+        </div>
 
                 <!-- New wrapper for links -->
-                <div class="link-group-wrapper">
+                <div class="link-group-wrapper ">
                     <div class="form-group link-group">
-                        <label for="github-link">
+                        <label for="github-link ">
                             <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" class="icon"> GitHub Link
                         </label>
                         <input type="url" id="github-link" name="github-link" value="<?php echo $edit['link1'] ?>" placeholder="GitHub Profile URL">
                     </div>
 
-                    <div class="form-group link-group">
+                    <div class="form-group link-group ">
                         <label for="linkedin-link">
                             <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn" class="icon"> LinkedIn Link
                         </label>
-                        <input type="url" id="linkedin-link" name="linkedin-link" value="<?php echo $edit['link2']?>" placeholder="LinkedIn Profile URL">
+                        <input type="url" id="linkedin-link" name="linkedin-link" value="<?php echo $edit['link2'] ?>" placeholder="LinkedIn Profile URL">
+                        
                     </div>
                 </div>
 
@@ -126,21 +133,12 @@ if(isset($_POST['update'])){
                         </div>
                       </button>
                     </div>
-                  </div>
+                </div>
             <?php } ?>
             </form>
         </div>
     </div>
+<script src="js/FREELANCERPROFILE.js"></script>
 
-    <script>
-        function previewImage(event) {
-            const reader = new FileReader();
-            reader.onload = function() {
-                const output = document.getElementById('image-preview');
-                output.src = reader.result;
-            }
-            reader.readAsDataURL(event.target.files[0]);
-        }
-    </script>
 </body>
 </html>
