9fa9faf6a1c1cc906332faf1ebd36b0d1eb0632b
Salmamohamed33 <140505053+Salmamohamed33@users.noreply.github.com>
2024-08-15 21:53:31 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$localhost= "localhost";
+$username= "root";
+$password= "";
+$database= "case2";
+
+$connect=mysqli_connect($localhost,$username,$password,$database);
+session_start();
+if(isset($_POST['logout'])){
+    session_unset();
+    session_destroy();
+    header("location:");
+}
+?>
