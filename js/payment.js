a7da52d1703582f82b480f8d4961e4c925e10d78
lailagabr <124739171+lailagabr@users.noreply.github.com>
2024-08-16 09:41:07 -0700

    Add files via upload

---
+++ js/payment.js
@@ -0,0 +1,12 @@
+const promoCode = document.querySelector('#promoCode')
+const showPromo = document.querySelector('#showPromo')
+
+    showPromo.addEventListener('click', function(){
+        promoCode.classList.toggle('d-none')
+       
+    })
+    
+    
+
+
+
