91d81a89af908679eec4004e1d4a9bb31d154095
ShehabSerry <111205286+ShehabSerry@users.noreply.github.com>
2024-08-17 12:45:48 +0300

    Add files via upload

--- login_client.php
+++ login_client.php
@@ -61,17 +61,20 @@ if(isset($_POST['login'])){
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <!----link bootsrap-->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
   <!-- link css -->
    <link rel='stylesheet' type='text/css'  media="screen" href="css/login.css"/>
     <title>Login</title>
   </head>
 
   <body> 
-    <a href="user_sign_up.php" class="close"><i class="fa-solid fa-x "></i></a>
+    <a href="user_sign_up.php" class="close"><i class="fa-solid fa-x"></i></a>
 <div class="background">
     <div class="container-main">
       <div class="wrapper">
+          <a href="home.php" class="close"><i class="fa-solid fa-x"></i></a>
           <div class="from-wraapper  Sign-in">
+
           <form method="post">
           <h2>Login</h2>
          
--- login_freelancer.php
+++ login_freelancer.php
@@ -56,6 +56,7 @@ if(isset($_POST['login'])){
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
          <!----link bootsrap-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
         <!-- link css -->
         <link rel='stylesheet' type='text/css'  media="screen" href="css/login.css"/>
         <title>Login</title>
