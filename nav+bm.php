5d7f25a26898a302a3fe211dec5acd322eefc923
ShehabSerry <111205286+ShehabSerry@users.noreply.github.com>
2024-08-19 05:50:32 +0300

    Update nav+bm.php

--- nav+bm.php
+++ nav+bm.php
@@ -26,7 +26,7 @@ if(isset($user_id))
 
 <head>
     <meta charset="utf-8">
-    <title>SEO Master </title>
+    <title>MiDlancer</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
