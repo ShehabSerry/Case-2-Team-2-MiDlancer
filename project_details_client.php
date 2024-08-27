e48ad4668ccf249299214284aa586af05901be3b
alaa <alaanaguib96@gmail.com>
2024-08-27 21:39:58 +0300

    Update project_details_client.php

--- project_details_client.php
+++ project_details_client.php
@@ -8,11 +8,11 @@ $details = $_GET['details'];
 // $user_id=2;
 
 
-$join="SELECT * FROM `team_member`
- JOIN `project` ON `project`.`project_id`=`team_member`.`project_id`
- JOIN `freelancer` ON `freelancer`.`freelancer_id`=`team_member`.`freelancer_id`
-JOIN `user` ON `user`.`user_id`=`project`.`user_id` 
-WHERE `project`.`project_id` = '$details'";
+$join= "SELECT * FROM `team_member`
+                JOIN `project` ON `project`.`project_id`=`team_member`.`project_id`
+                JOIN `freelancer` ON `freelancer`.`freelancer_id`=`team_member`.`freelancer_id`
+                JOIN `user` ON `user`.`user_id`=`project`.`user_id` 
+        WHERE `project`.`project_id` = '$details'";
 
 
 $run_join=mysqli_query($connect,$join);
@@ -55,25 +55,21 @@ $run_join=mysqli_query($connect,$join);
             <h3>Team Member: <span class="ag-courses-item_date">
             <?php echo $data['freelancer_name']?>
               </span>
-              <span><img src="img/<?php echo $data['freelancer_image']?>" alt=""></span>
+              <span><img src="img/<?php echo $data['freelancer_image']?>" alt="team member img"></span>
             </h3>
-
           </div>
           <div class="ag-courses-item_date-box">
             <!-- <i class="fa-solid fa-money-bills"></i> -->
             <h3>Status: <span class="ag-courses-item_date">
             <?php echo $data ['status']?>
-              </span></h3>
-
+              </span>
+            </h3>
           </div>
 
-          
+          </a>  
         <?php } ?>
       </div>
       
-
-
-
 
     </div>
   </div>
