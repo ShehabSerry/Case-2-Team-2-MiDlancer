60e41a80fe0ac06809c1ac2ec93c4b976a4ca8fb
alaa <alaanaguib96@gmail.com>
2024-08-27 18:50:14 +0300

    Update display_calendar.php

--- display_calendar.php
+++ display_calendar.php
@@ -1,6 +1,8 @@
 <?php             
 include("connection.php");
-
+if(isset($_SESSION['freelancer_id'])){
+    $freelancer_id = $_SESSION['freelancer_id'];
+}
 // $display_query = "SELECT `plan`.`plan_name`,`project`.`project_name`, `project`.`deadline_date`, `subscription`.`end_date`
 //  FROM `project` 
 // join `team_member` on `project`.`project_id`=`team_member`.`project_id`
@@ -14,7 +16,7 @@ join `team_member` on `project`.`project_id`=`team_member`.`project_id`
 join `freelancer` on `freelancer`.`freelancer_id`=`team_member`.`freelancer_id`
 -- join `subscription` on `freelancer`.`freelancer_id`= `subscription`.`freelancer_id`
 -- join `plan` on `subscription`.`plan_id`= `plan`.`plan_id`
-where `freelancer`.`freelancer_id`=15 
+where `freelancer`.`freelancer_id`= '$freelancer_id' 
 -- GROUP BY `plan`.`plan_id`
 ";
             
@@ -23,7 +25,7 @@ $run = mysqli_query($connect, $display_query);
 
 $subs="SELECT * FROM subscription  
 join `plan` on `subscription`.`plan_id`= `plan`.`plan_id`
-where freelancer_id=15";
+where freelancer_id= '$freelancer_id'";
 $runS = mysqli_query($connect, $subs);   
 $fetch=mysqli_fetch_array($runS);
 $plan_name= $fetch['plan_name'];
