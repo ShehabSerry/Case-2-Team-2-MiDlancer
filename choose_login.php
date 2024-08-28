488c5863efd09a21d6dc4c6fe79a0f0b21c05b64
Salmamohamed33 <140505053+Salmamohamed33@users.noreply.github.com>
2024-08-28 15:30:08 +0300

    Add files via upload

---
+++ choose_login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/signups.css">  
+</head>
+<body >
+    <div class="main">
+        <div class="wrapper">
+          <div class="card-border-top">
+          </div>
+          <h2>Freelancer Login</h2>
+          <div class="buttons ">
+          <a href="login_freelancer.php">
+            <button class="cssbuttons-io-button">
+               Get started
+              <div class="icon">
+                  <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 0h24v24H0z" fill="none"></path>
+                      <path
+                          d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                          fill="currentColor"></path>
+                  </svg>
+              </div>
+          </button>
+          </a>
+          </div>
+        
+        </div>
+        
+        
+        <div class="wrapper">
+          <div class="card-border-top">
+          </div>
+          <h2>Client Login</h2>
+          <div class="buttons ">
+          <a href="login_client.php">
+            <button class="cssbuttons-io-button">
+              Get started
+              <div class="icon">
+                  <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 0h24v24H0z" fill="none"></path>
+                      <path
+                          d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                          fill="currentColor"></path>
+                  </svg>
+              </div>
+          </button>
+          </a>
+          </div>
+        
+        </div>
+        </div>
+         
+</body>
+</html>
