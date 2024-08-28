e1d3a878899e8c5e2f4980e1b35f7abfa8e9c4e1
alaa <alaanaguib96@gmail.com>
2024-08-28 15:13:05 +0300

    Update display_admins.php

--- admin/display_admins.php
+++ admin/display_admins.php
@@ -34,7 +34,7 @@ if (isset($_GET['delete'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
     integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- <link rel="stylesheet" href="css/deleteadmin.css" /> -->
+    <link rel="stylesheet" href="css/displayfreelancers.css" />
     <style>
         /* Popup styling */
         .popup {
