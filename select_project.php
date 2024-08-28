7cf59c6a8064e05277564abdd32283aae30ddc55
Youssab11 <youssabking2@gmail.com>
2024-08-28 19:44:24 +0300

    Add files via upload

--- select_project.php
+++ select_project.php
@@ -29,6 +29,12 @@ if(isset($_GET['vfid']))
         }
     }
 }
+
+if (isset($_POST['addproject'])){
+  header("Location: addproject.php");
+
+
+}
 ?>
 <html lang="en">
 
@@ -69,6 +75,17 @@ if(isset($_GET['vfid']))
                 <div class="buttons">
                   <button class="cssbuttons-io-button addto" name="submit">
                     <a href="#">Add To Team</a>
+                    <div class="icon">
+                      <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                          fill="currentColor"></path>
+                      </svg>
+                    </div>
+                    
+                  </button>
+                  <button class="cssbuttons-io-button addto" name="addproject">
+                    <a href="addproject.php">Add Project</a>
                     <div class="icon">
                       <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 0h24v24H0z" fill="none"></path>
