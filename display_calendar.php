39f658c285035cb06e4e1d048f1577e22d7765ab
alaa <alaanaguib96@gmail.com>
2024-08-29 21:42:30 +0300

    Update display_calendar.php

--- display_calendar.php
+++ display_calendar.php
@@ -3,22 +3,12 @@ include("connection.php");
 if(isset($_SESSION['freelancer_id'])){
     $freelancer_id = $_SESSION['freelancer_id'];
 }
-// $display_query = "SELECT `plan`.`plan_name`,`project`.`project_name`, `project`.`deadline_date`, `subscription`.`end_date`
-//  FROM `project` 
-// join `team_member` on `project`.`project_id`=`team_member`.`project_id`
-// join `freelancer` on `freelancer`.`freelancer_id`=`team_member`.`freelancer_id`
-//  join `subscription` on `freelancer`.`freelancer_id`= `subscription`.`freelancer_id`
-// join `plan` on `subscription`.`plan_id`= `plan`.`plan_id`
-// where `freelancer`.`freelancer_id`=15"; 
+
 
 $display_query = "SELECT * FROM `project` 
 join `team_member` on `project`.`project_id`=`team_member`.`project_id`
 join `freelancer` on `freelancer`.`freelancer_id`=`team_member`.`freelancer_id`
--- join `subscription` on `freelancer`.`freelancer_id`= `subscription`.`freelancer_id`
--- join `plan` on `subscription`.`plan_id`= `plan`.`plan_id`
-where `freelancer`.`freelancer_id`= '$freelancer_id' 
--- GROUP BY `plan`.`plan_id`
-";
+where `freelancer`.`freelancer_id`= '$freelancer_id' ";
             
 $run = mysqli_query($connect, $display_query);   
 
@@ -39,9 +29,8 @@ if ($count > 0) {
     foreach($run as $data_row) {	
         $data_arr[$i]['task_name'] = $data_row['project_name'];
         $data_arr[$i]['deadline_date'] = date("Y-m-d", strtotime($data_row['deadline_date']));
-        // $data_arr[$i]['plan_name'] = $data_row['plan_name'];
-        // $data_arr[$i]['end_date'] = date("Y-m-d", strtotime($data_row['end_date']));
         $data_arr[$i]['color'] = '#0A7273'; 
+        $data_arr[$i]['project_url'] = 'project_details_freelancer.php?details=' . $data_row['project_id']; 
         $i++;
     }
 	$data_arr[]=['task_name'=>$plan_name, 'deadline_date'=>$planEndDate];
