0583f321338912a9a246ec6501fd9fefae7af0cf
alaa <alaanaguib96@gmail.com>
2024-08-29 23:50:59 +0300

    Update navbarr.php

--- navbarr.php
+++ navbarr.php
@@ -16,26 +16,31 @@
         <div class="navbar-links">
             <ul>
                 <li><a href="home.php">Home</a></li>
-                <?php
-                if(isset($_SESSION['user_id'])){ ?>
-                        <li><a href="clientprofile.php">Profile</a></li>
-                       <?php } elseif(isset($_SESSION['freelancer_id'])){ ?>
-                       <li><a href="FREELANCERPROFILE.php">Profile</a></li>
-                        <?php } ?>
-            
                 <li><a href="career.php">career</a></li>
-                <?php 
-                if(isset($_SESSION['user_id'])){ ?>                       
-                        <li><a href="accepted-requests.php">Notifications</a></li>
-                        <?php } ?>
-            
                 <li><a href="wall.php">wall</a></li>
                 <?php
                   if(isset($_SESSION['user_id'])){ ?>
                     <li><a href="my_projects_client.php">Projects</a></li>
-                 <?php } elseif(isset($_SESSION['freelancer_id'])){ ?>
-                  <li><a href="my_projects_freelancer.php">Projects</a></li>
-                  <?php } ?>
+                    <li><a href="clientprofile.php">Profile</a></li>
+                    <li><a href="accepted-requests.php">Notifications</a></li>
+                <?php } elseif(isset($_SESSION['freelancer_id'])){ ?>
+                    <li><a href="my_projects_freelancer.php">Projects</a></li>
+                    <li><a href="FREELANCERPROFILE.php">Profile</a></li>
+                    <li><a href="./income-request.php">Notifications</a></li>
+                <?php } ?>
+                
+                <div class="nav-item dropdown">
+                    <?php   if(isset($LI_F_id)){ ?>
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+
+                    <div class="dropdown-menu m-0">
+                        <a href="job_postings.php" class="dropdown-item">Job Postings</a>
+                        <a href="calendar.html" class="dropdown-item">Calendar</a>
+                        <a href="dashboard.php" class="dropdown-item">Dashboard</a>
+                    </div>
+                    <?php }?>
+                </div>
+             
             </ul>
         </div>
         <a href="#" class="toggle-button">
