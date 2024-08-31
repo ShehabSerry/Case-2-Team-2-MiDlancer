4a207de1ab458dda333bdce15c7ee51a5a4f997b
Bushra-Hazem <bushrahazem233@gmail.com>
2024-08-31 17:34:47 +0300

    Add files via upload

---
+++ features.php
@@ -0,0 +1,242 @@
+<?php
+include 'nav+bm.php';
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>midlancer</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Jost:wght@500;600;700&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/features.css" rel="stylesheet">
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="51">
+    <div class="container-xxl bg-white p-0 pt-5">
+            <!-- Process Start -->
+            <div class="container-xxl ">
+             <div class="container  px-lg-5">
+                 <div class="text-center pb-4 wow fadeInUp" data-wow-delay="0.1s">
+                     <h5 class="text-primary-gradient fw-medium">How It Works</h5>
+                     <h1 class="mb-5">3 Easy Steps</h1>
+                 </div>
+                 <div class="row gy-5 gx-4 justify-content-center">
+                     <div class="col-lg-4 col-sm-6 text-center pt-4 wow fadeInUp" data-wow-delay="0.1s">
+                         <div class="position-relative bg-light rounded pt-5 pb-4 px-4">
+                             <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle position-absolute top-0 start-50 translate-middle shadow" style="width: 100px; height: 100px;">
+                                 <i class="fa fa-cog fa-3x text-white"></i>
+                             </div>
+                             <h5 class="mt-4 mb-3">sign-up</h5>
+                             <p class="mb-0">join our platform and create your account to start connecting with top freelancers </p>
+                         </div>
+                     </div>
+                     <div class="col-lg-4 col-sm-6 text-center pt-4 wow fadeInUp" data-wow-delay="0.3s">
+                         <div class="position-relative bg-light rounded pt-5 pb-4 px-4">
+                             <div class="d-inline-flex align-items-center justify-content-center bg-secondary-gradient rounded-circle position-absolute top-0 start-50 translate-middle shadow" style="width: 100px; height: 100px;">
+                                 <i class="fa fa-address-card fa-3x text-white"></i>
+                             </div>
+                             <h5 class="mt-4 mb-3">Setup Your Profile</h5>
+                             <p class="mb-0">build your profile, show case your skills and heighligh your experience to attract potential clients. </p>
+                         </div>
+                     </div>
+                     <div class="col-lg-4 col-sm-6 text-center pt-4 wow fadeInUp" data-wow-delay="0.5s">
+                         <div class="position-relative bg-light rounded pt-5 pb-4 px-4">
+                             <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle position-absolute top-0 start-50 translate-middle shadow" style="width: 100px; height: 100px;">
+                                 <i class="fa fa-check fa-3x text-white"></i>
+                             </div>
+                             <h5 class="mt-4 mb-3">Enjoy The Features</h5>
+                             <p class="mb-0">interview potential fits fo  freelancers to connect, share knowledge, and collaborate.</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <!-- Process Start -->
+     
+     
+     
+
+
+
+       <!-- Features Start -->
+       <div class="container-xxl py-5" id="feature">
+        <div class="container py-5 px-lg-5">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h5 class="text-primary-gradient fw-medium">website Features</h5>
+                <h1 class="mb-5">Awesome Features</h1>
+            </div>
+            <div class="row g-4">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="feature-item bg-light rounded p-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 60px; height: 60px;">
+                            <i class="fa fa-eye text-white fs-4"></i>
+                        </div>
+                        <h5 class="mb-3">Advanced Search</h5>
+                        <p class="m-0">Clients can search for freelancers based on skills, experience, ratings, and location.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="feature-item bg-light rounded p-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-secondary-gradient rounded-circle mb-4" style="width: 60px; height: 60px;">
+                            <i class="fa fa-layer-group text-white fs-4"></i>
+                        </div>
+                        <h5 class="mb-3">Portfolio Upload</h5>
+                        <p class="m-0">Freelancers can upload samples of their work in various formats (images,  documents)</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="feature-item bg-light rounded p-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 60px; height: 60px;">
+                            <i class="fa fa-edit text-white fs-4"></i>
+                        </div>
+                        <h5 class="mb-3">Feedback System</h5>
+                        <p class="m-0">Clients can leave reviews and rate freelancers based on their work with them.  </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="feature-item bg-light rounded p-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-secondary-gradient rounded-circle mb-4" style="width: 60px; height: 60px;">
+                            <i class="fa fa-shield-alt text-white fs-4"></i>
+                        </div>
+                        <h5 class="mb-3">Community Forums</h5>
+                        <p class="m-0">A space for freelancers to connect, share knowledge, and collaborate.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="feature-item bg-light rounded p-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 60px; height: 60px;">
+                            <i class="fa fa-cloud text-white fs-4"></i>
+                        </div>
+                        <h5 class="mb-3">Task Management</h5>
+                        <p class="m-0">Tools for freelancers to track premium deadline</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="feature-item bg-light rounded p-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-secondary-gradient rounded-circle mb-4" style="width: 60px; height: 60px;">
+                            <i class="fa fa-mobile-alt text-white fs-4"></i>
+                        </div>
+                        <h5 class="mb-3">Responsive Design</h5>
+                        <p class="m-0">Ensuring the website is mobile-friendly for access on various devices</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Features End -->
+
+        <!-- Footer Start -->
+        <div class="container-fluid pt-5 text-light footer  wow fadeIn" data-wow-delay="0.1s">
+            <div class="haha"></div>
+            <div class="haha pt-5">
+                <div class="container pt-5 px-lg-5">
+                    <div class="row g-5">
+                        <div class="col-md-6 col-lg-3">
+                            <h5 class="text-white mb-4">Get In Touch</h5>
+                            <p><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+                            <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                            <p><i class="fa fa-envelope me-3"></i>MiDlancerTeam@gmail.com</p>
+                            <div class="d-flex pt-2">
+                                
+                                <a target="_blank" class="btn btn-outline-light btn-social" href="https://www.facebook.com/profile.php?id=61564326657962&mibextid=ZbWKwL"><i
+                                        class="fab fa-facebook-f"></i></a>
+                               
+                                <a target="_blank"  class="btn btn-outline-light btn-social" href="https://www.instagram.com/midlancer.1/"><i class="fab fa-instagram"></i></a>
+                                <a target="_blank"  class="btn btn-outline-light btn-social" href="https://www.tiktok.com/@midlancer5?_t=8oxRnoCQ62H&_r=1"><i
+                                     class="fa-brands fa-tiktok"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <h5 class="text-white mb-4">Popular Link</h5>
+                            <a class="btn btn-link" href="">About Us</a>
+                            <a class="btn btn-link" href="">Contact Us</a>
+                           
+                            <a class="btn btn-link" href="">Terms & Condition</a>
+                            <a class="btn btn-link" href="career.php">Career</a>
+                        </div>
+
+                        <div class="col-md-6 col-lg-6 mt-auto pe-5  mb-4">
+                            <h5 class="text-white mb-4">Newsletter</h5>
+                            <p>Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                                velit non vulpu</p>
+                                <a href="choose.php">
+                                <!-- <button class="cssbuttons-io-button">
+                                Get started
+                                <div class="icon">
+                                        <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 0h24v24H0z" fill="none"></path>
+                                            <path
+                                                d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                                                fill="currentColor"></path>
+                                        </svg>
+                                    </div>
+                                </button> -->
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="container px-lg-5">
+                    <div class="copyright">
+                        <div class="row">
+                            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                                &copy; <a class="border-bottom" href="#">MID LANCER</a>, All Right Reserved.
+                                Designed By <a class="border-bottom" href="">team 2</a>
+                            </div>
+                            <div class="col-md-6 text-center text-md-end">
+                                <div class="footer-menu">
+                                    <a href="">Home</a>
+                                    <a href="">Cookies</a>
+                                    <a href="">Help</a>
+                                    <a href="">FQAs</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- Footer End -->
+    
+
+</div>
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/wow/wow.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/counterup/counterup.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
+</body>
+
+</html>
