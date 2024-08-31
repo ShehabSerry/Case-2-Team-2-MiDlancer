d1b5bee9330e69afb787e67d720412dfc556ada8
malakesslam <malakesslam88@gmail.com>
2024-08-31 19:08:40 +0300

    Add files via upload

--- payment_history.php
+++ payment_history.php
@@ -1,11 +1,12 @@
 <?php
-// include 'nav+bm.php';
+
 include("connection.php");
-if(isset($_SESSION['user_id'])){
-    $user_id = $_SESSION['user_id'];
-}else{
-    header("location:home.php");
-}
+// if(isset($_SESSION['user_id'])){
+//     $user_id = $_SESSION['user_id'];
+// }else{
+//     header("location:home.php");
+// }
+$user_id = $_SESSION['user_id'];
 $history = "SELECT `project`.*, 
                     `user`.*,
                     `freelancer`.*, 
@@ -20,36 +21,55 @@ $result = mysqli_query($connect,$history);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+     <!-- fontaswomn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- bootstrab link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css
+    ">
+    <!-- css -->
+    <link rel="stylesheet" href="css/payment_history.css">
+
     <title>Payment History</title>
 </head>
 <body>
-    <h1>Payment History</h1>
+<?php  include("navbarr.php"); ?>  
+<h1 class="text-center my-5 text-primary ">
+Payment History
+</h1>
+<div class="all">
 
-    <table>
-        <thead>
-            <tr>
-                <th>Project Name</th>
-                <th>Freelancer Name</th>
-                <th>Total Amount</th>
-                <th>Payment Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if (mysqli_num_rows($result) > 0) { ?>
-                <?php foreach ($result as $row) { ?>
-                    <tr>
-                    <td><?php echo htmlspecialchars($row["project_name"]) ?></td>;
-                    <td><?php echo htmlspecialchars($row["freelancer_name"]) ?></td>;
-                    <td><?php echo htmlspecialchars($row["amount"]) ?></td>;
-                    <td><?php echo date("d-m-Y", strtotime($row["date"])) ?></td>;
-                    </tr>
-                <?php } ?>
-            <?php } else { ?>
-                <td>No payment yet.</td>
-            <?php } ?>
-        </tbody>
-    </table>
+
+    
+<table class="table table-striped w-90">
+    <thead>
+        <tr>
+            <th><h3>Project Name</h3></th>
+            <th><h3>Freelancer Name</h3></th>
+            <th><h3>Total Amount</h3></th>
+            <th><h3>Payment Date</h3></th>
+          
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach($result as $data){
+        ?> 
+        <tr>
+            <td><?php echo htmlspecialchars($data["project_name"]) ?></td>
+            <td><?php echo htmlspecialchars($data["freelancer_name"]) ?></td>
+            <td><?php echo htmlspecialchars($data["amount"]) ?></td>
+            <td><?php echo date("d-m-Y", strtotime($data["date"])) ?></td>
+        </tr>
+        <?php } ?>
+    </tbody>
+</table>
+</div>
 </body>
 </html>
