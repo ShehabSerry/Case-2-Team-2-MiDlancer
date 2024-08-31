dcf77ea0a760d66815d77c55aa38f140963231ad
alaa <alaanaguib96@gmail.com>
2024-08-31 16:07:54 +0300

    Update freelancers.php

--- freelancers.php
+++ freelancers.php
@@ -96,6 +96,7 @@ if(isset($_GET['details'])) // bushra
         else
         {
             $error = "Request has already been sent";
+            $popup = true;
         }
     }
 }
@@ -118,6 +119,292 @@ if (isset($_POST['get_drop_down']))
               integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
         <link rel="stylesheet" href="css/freelancers.css">
+        <style>
+        /* Additional CSS for the overlay */
+        :root{
+    --white: #fcfcfc;
+    --gray: #cbcdd3;
+    --dark: #777777;
+    --error: #ef8d9c;
+    --orange: #ffc39e;
+    --success: #b0db7d;
+    --secondary: #99dbb4;
+}
+
+
+@import url("https://fonts.googleapis.com/css?family=Lato:400,700");
+
+/* $font: "Lato", sans-serif; */
+
+
+
+.containerr {
+  position: relative;
+  margin: auto;
+  overflow: hidden;
+  width: 700px;
+  height: 250px;
+} 
+
+#containerr {
+  position: relative;
+  margin: auto;
+  overflow: hidden;
+  width: 700px;
+  height: 250px;
+} 
+
+h1 {
+  font-size: 0.9em;
+  font-weight: 100;
+  letter-spacing: 3px;
+  padding-top: 5px;
+  color: var(--white) ;
+  padding-bottom: 5px;
+  text-transform: uppercase;
+}
+
+.green {
+  color:var(--secondary);
+   /* darken($secondary, 20%); */
+}
+
+.red {
+  color: var(--error);
+  /* darken($error, 10%); */
+}
+
+.alert {
+  font-weight: 700;
+  letter-spacing: 5px;
+}
+
+p {
+  margin-top: -5px;
+  /* font-size: 0.5em; */
+  /* font-weight: 100; */
+  color: var(--white);
+  /* darken($dark, 10%); */
+  letter-spacing: 1px;
+}
+
+button,
+.dot {
+  cursor: pointer;
+}
+
+#success-box {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  right: 0;
+  background: linear-gradient(to bottom right, var(--success) , var(--secondary) );
+  border-radius: 20px;
+  box-shadow: 5px 5px 20px rgba(var(--gray), 10%);
+  perspective: 40px;
+}
+
+#error-box {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  right: 0;
+  background: linear-gradient(to bottom left, var(--error) 40%, var(--orange) 100%);
+  border-radius: 20px;
+  box-shadow: 5px 5px 20px rgba(var(--gray), 10%);
+}
+
+.dot {
+  width: 8px;
+  height: 8px;
+  background: var(--white);
+  border-radius: 50%;
+  position: absolute;
+  top: 4%;
+  right: 6%;
+
+}
+.dot:hover {
+    background: darken(var(--white), 20%);
+  }
+
+.two {
+  right: 12%;
+  opacity: 0.5;
+}
+
+.face {
+  position: absolute;
+  width: 22%;
+  height: 22%;
+  background: var(--white);
+  border-radius: 50%;
+  border: 1px solid var(--dark);
+  top: 21%;
+  left: 37.5%;
+  z-index: 2;
+  animation: bounce 1s ease-in infinite;
+}
+
+.face2 {
+  position: absolute;
+  width: 22%;
+  height: 22%;
+  background: var(--white);
+  border-radius: 50%;
+  border: 1px solid var(--dark);
+  top: 21%;
+  left: 37.5%;
+  z-index: 2;
+  animation: roll 3s ease-in-out infinite;
+}
+
+.eye {
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  background: var(--dark);
+  border-radius: 50%;
+  top: 40%;
+  left: 20%;
+}
+
+.right {
+  left: 68%;
+}
+
+.mouth {
+  position: absolute;
+  top: 43%;
+  left: 41%;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+}
+
+.happy {
+  border: 2px solid;
+  border-color: transparent var(--dark) var(--dark) transparent;
+  transform: rotate(45deg);
+}
+
+.sad {
+  top: 49%;
+  border: 2px solid;
+  border-color: var(--dark) transparent transparent var(--dark);
+  transform: rotate(45deg);
+}
+
+.shadow {
+  position: absolute;
+  width: 21%;
+  height: 3%;
+  opacity: 0.5;
+  background: var(--dark);
+  left: 40%;
+  top: 43%;
+  border-radius: 50%;
+  z-index: 1;
+}
+
+.scale {
+  animation: scale 1s ease-in infinite;
+}
+.move {
+  animation: move 3s ease-in-out infinite;
+}
+
+.message {
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  height: 40%;
+  top: 47%;
+  
+}
+
+.button-box {
+  position: absolute;
+  background: var(--white);
+  width: 50%;
+  height: 15%;
+  border-radius: 20px;
+  top: 73%;
+  left: 25%;
+  outline: 0;
+  border: none;
+  box-shadow: 2px 2px 10px rgba(var(--dark), 0.5);
+  transition: all 0.5s ease-in-out;
+}
+.button-box:hover {
+    /* background: darken(var(--white), 5%); */
+    transform: scale(1.05);
+    transition: all 0.3s ease-in-out;
+  }
+
+@keyframes bounce {
+  50% {
+    transform: translateY(-10px);
+  }
+}
+
+@keyframes scale {
+  50% {
+    transform: scale(0.9);
+  }
+}
+
+@keyframes roll {
+  0% {
+    transform: rotate(0deg);
+    left: 25%;
+  }
+  50% {
+    left: 60%;
+    transform: rotate(168deg);
+  }
+  100% {
+    transform: rotate(0deg);
+    left: 25%;
+  }
+}
+
+@keyframes move {
+  0% {
+    left: 25%;
+  }
+  50% {
+    left: 60%;
+  }
+  100% {
+    left: 25%;
+  }
+}
+
+        .overlay {
+            display: none;
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            z-index: 999;
+        }
+        .popup {
+            display: none;
+            position: fixed;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            width: 300px;
+            background-color: #fff;
+            border-radius: 20px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            z-index: 1000;
+            height: 50%;
+        }
+    </style>
     </head>
 
 <body >
@@ -278,6 +565,47 @@ if (isset($_POST['get_drop_down']))
             <?php }} ?>
         </ul>
     </nav>
+
+
+        <!-- Overlay and Popup HTML -->
+
+
+<div class="overlay" id="overlay" onclick="closePopup()"></div>
+    <div class="container popup" id="popup">
+        <div id="error-box">
+            <div class="dot"></div>
+            <div class="dot two"></div>
+            <div class="face2">
+                <div class="eye"></div>
+                <div class="eye right"></div>
+                <div class="mouth sad"></div>
+            </div>
+            <div class="shadow move"></div>
+            <div class="message">
+                <h1 class="alert">Error!</h1>
+                <p>Request has already been sent.</p>
+            </div>
+                <h1 class="red">try again</h1>
+            </button>
+        </div>
+    </div>
+
+    <script>
+        function openPopup() {
+            document.getElementById('popup').style.display = 'block';
+            document.getElementById('overlay').style.display = 'block';
+        }
+
+        function closePopup() {
+            document.getElementById('popup').style.display = 'none';
+            document.getElementById('overlay').style.display = 'none';
+        }
+
+        <?php if ($popup){ ?>
+        // Automatically open the popup if $popup is true
+        openPopup();
+        <?php } ?>
+        </script>
 </body>
 
 </html>
