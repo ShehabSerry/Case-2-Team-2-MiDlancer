c2a656c8d0a273e386af5e7f3ca7672091f8874a
Salmamohamed33 <140505053+Salmamohamed33@users.noreply.github.com>
2024-09-01 23:31:41 +0300

    Update EDITPROFILE_freelancer.php

--- EDITPROFILE_freelancer.php
+++ EDITPROFILE_freelancer.php
@@ -352,7 +352,7 @@ button,
             top: 50%;
             transform: translate(-50%, -50%);
             width: 300px;
-            background-color: #fff;
+           
             border-radius: 8px;
             box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
           
