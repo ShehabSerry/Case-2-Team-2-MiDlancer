d9eef540a6fdd9e9982aea82bfa828ed6696dc44
alaa <alaanaguib96@gmail.com>
2024-09-02 04:05:29 +0300

    Update navbarr.php

--- navbarr.php
+++ navbarr.php
@@ -22,11 +22,11 @@
                   if(isset($_SESSION['user_id'])){ ?>
                     <li><a href="my_projects_client.php">Projects</a></li>
                     <li><a href="clientprofile.php">Profile</a></li>
-                    <li><a href="accepted-requests.php">Notifications</a></li>
+                    <li><a href="accepted-requests.php"><i class="fa-solid fa-bell" style="color: #f6d673;"></i></a></li>
                 <?php } elseif(isset($_SESSION['freelancer_id'])){ ?>
                     <li><a href="my_projects_freelancer.php">Projects</a></li>
                     <li><a href="FREELANCERPROFILE.php">Profile</a></li>
-                    <li><a href="./income-request.php">Notifications</a></li>
+                    <li><a href="./income-request.php"><i class="fa-solid fa-bell" style="color: #f6d673;"></i></a></li>
                 <?php } ?>
                 
                 <div class="nav-item dropdown">
